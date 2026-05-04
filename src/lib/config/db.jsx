21f0a97133d22e42b52c91c0eaf9619058a620ed
Azmain Mahtab <145844907+Azmain2005@users.noreply.github.com>
2026-05-04 20:32:30 +0600

    Delete src directory

--- src/lib/config/db.jsx
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB = async () =>{
-    await mongoose.connect('mongodb+srv://azmain2005mahtab_db_user:Azmain.2005@cluster0.6klkamu.mongodb.net/StoreDb');
-    console.log ("database connected");
-}
